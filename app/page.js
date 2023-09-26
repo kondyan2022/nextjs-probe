46e2dc8d863bf00b4564e08dd9dd49ba22af1a73
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-09-27 00:37:30 +0300

    filename fix

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Hero from "@/sections/Hero/Hero";
 import Gallery from "@/sections/Gallery/Gallery";
 import Contact from "@/sections/Contact/contact";
 import Offer from "@/sections/Offer/Offer";
-import About from "@/sections/About/About";
+import About from "@/sections/About/about";
 import ChooseUs from "@/sections/ChooseUs/ChooseUs";
 
 export default function Home() {
